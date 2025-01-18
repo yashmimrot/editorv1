72c99a2f0c1d91aee46fcd65f6d2b9015428e851
yashmimrot@gmail.com <yash.mimrot@clear.in>
2025-01-18 19:28:12 +0530

    set repository

---
+++ src/main/java/videverse/editorv1/Editorv1Application.java
@@ -0,0 +1,13 @@
+package videverse.editorv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Editorv1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Editorv1Application.class, args);
+	}
+
+}
